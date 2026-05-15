b888b46ed4d401fd335c0654a29d01e56fbde704
GUnzner <grit.unzner@googlemail.com>
2026-05-15 15:09:11 +0200

    changed backend config to azure cosmos db

--- config.py
+++ config.py
@@ -1,17 +1,14 @@
 from flask import Flask
-from flask_login import UserMixin, current_user, login_manager
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
+from azure.cosmos import CosmosClient, exceptions
 import os
 
 app = Flask(__name__,  template_folder='../templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = '#FIXME DATABASE_URL'
-db = SQLAlchemy(app)
 
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", os.urandom(32))
+
 csrf = CSRFProtect(app)
 csrf.init_app(app)
 bcrypt = Bcrypt(app)
@@ -19,18 +16,43 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+# Azure Cosmos DB config
+COSMOS_ENDPOINT = os.environ["COSMOS_ENDPOINT"]
+COSMOS_KEY = os.environ["COSMOS_KEY"]
+DATABASE_NAME = "your_database"
+CONTAINER_NAME = "users"
+
+# Initialize Cosmos DB
+client = CosmosClient(COSMOS_ENDPOINT, credential=COSMOS_KEY)
+database = client.get_database_client(DATABASE_NAME)
+container = database.get_container_client(CONTAINER_NAME)
+
+class User(UserMixin):
+    def __init__(self, id, username, email, password):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
+
+    @staticmethod
+    def get(user_id):
+        try:
+            user_data = container.read_item(
+                item=str(user_id),
+                partition_key=str(user_id)
+            )
+
+            return User(
+                id=user_data["id"],
+                username=user_data["username"],
+                email=user_data["email"],
+                password=user_data["password"]
+            )
+
+        except exceptions.CosmosResourceNotFoundError:
+            return None
+
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, 
-               primary_key = True)
-    username = db.Column(db.String(20),
-                         unique = True, 
-                         nullable = False)
-    email = db.Column(db.String(120), 
-                      unique = True, 
-                      nullable = False)
-    password = db.Column(db.String(60), 
-                         nullable = False)
+    return User.get(user_id)
